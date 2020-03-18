5a6520379e244fb6743348c1233accc68a63c356
MarkWatney15102 <42077358+MarkWatney15102@users.noreply.github.com>
2020-03-18 19:35:11 +0100

    First Push

---
+++ config/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace config\Config;
+
+class Config
+{
+    public function __construct()
+    {
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+$includeDir = [
+
+];
